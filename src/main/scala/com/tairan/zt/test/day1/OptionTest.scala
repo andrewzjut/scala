05d2d2fee3da1fdb7380eb4277be1f49f41faf7a
zt <hzzt@tairanchina.com>
2016-07-11 20:01:34 +0800

    test1

---
+++ src/main/scala/com/tairan/zt/test/day1/OptionTest.scala
@@ -0,0 +1,15 @@
+package com.tairan.zt.test.day1
+
+/**
+ * Created by hzzt on 2016/7/11.
+ */
+object OptionTest extends App {
+  val stateCapitals = Map(
+    "alabama" -> "mantgomery",
+    "alaska" -> "juneau",
+    "wyoming" -> "cheyenne"
+  )
+
+  println(stateCapitals.get("alabama").get)
+  println(stateCapitals.get("Unknow").getOrElse("oops"))
+}

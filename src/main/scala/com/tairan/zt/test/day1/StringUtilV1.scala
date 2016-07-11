c78042e962425370b173279c35a31012dd85f5f8
zt <hzzt@tairanchina.com>
2016-07-11 19:07:45 +0800

    test

--- src/main/scala/com/tairan/zt/test/day1/StringUtilV1.scala
+++ src/main/scala/com/tairan/zt/test/day1/StringUtilV1.scala
@@ -6,7 +6,17 @@ package com.tairan.zt.test.day1
 object StringUtilV1 {
   def joiner(strings:String*):String = strings.mkString("-")
   def joiner(strings:List[String]):String= joiner(strings:_*)
+
+  def makeList(strings:String*) ={
+    if (strings.length==0)
+      Nil
+    else
+      strings.toList
+  }
   def main(args: Array[String]): Unit = {
     println( StringUtilV1.joiner(List("Programming", "Scala")) )
+
+    val list:List[String]=makeList("zhangtong","is a good boy")
+    list.foreach(println(_))
   }
 }
